c40c2aa5080187209c059df5fd2631209a57f00a
moldyn-nagel <daniel.nagel@physik.uni-freiburg.de>
2020-01-07 11:10:29 +0100

    Update tests for io module.

--- test/test_iotext.py
+++ test/test_iotext.py
@@ -13,9 +13,11 @@ TODO:
 """
 # ~~~ IMPORT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import os.path
-import pytest
-import msmhelper
+
 import numpy as np
+import pytest
+
+import msmhelper
 
 # Current directory
 HERE = os.path.dirname(__file__)
@@ -53,22 +55,32 @@ def test_savetxt(traj_file, header, tmpdir):
     assert (msmhelper.opentxt(output) == expected).all()
 
 
-def test_open_limits(limits_file):
+@pytest.mark.parametrize('wrong_limit', [os.path.join(HERE, 'data.dat')])
+def test_open_limits(limits_file, wrong_limit):
     """Test that the limits are loaded correctly."""
     assert (msmhelper.open_limits(13, limits_file) == [5, 10, 13]).all()
     assert (msmhelper.open_limits(13) == [13]).all()
+    with pytest.raises(msmhelper.FileError):
+        msmhelper.open_limits(13, wrong_limit)
+    with pytest.raises(ValueError):
+        msmhelper.open_limits(14, limits_file)
 
 
 @pytest.mark.parametrize('traj_file, kwargs', [
     (os.path.join(HERE, 'traj1.dat'), {}),
     (os.path.join(HERE, 'traj1.dat'), {'dtype': np.integer}),
-    (os.path.join(HERE, 'traj1.dat'), {'dtype': np.float})])
+    (os.path.join(HERE, 'traj1.dat'), {'dtype': np.float}),
+    (os.path.join(HERE, 'data.dat'), {'no_traj': True})])
 def test_opentxt_limits(limits_file, traj_file, kwargs):
     """Test that the trajectory is split correctly."""
     if 'dtype' in kwargs and kwargs['dtype'] == np.float:
-        with pytest.raises(AssertionError):
+        with pytest.raises(TypeError):
             msmhelper.opentxt_limits(limits_file=limits_file,
                                      file_name=traj_file, **kwargs)
+    elif 'no_traj' in kwargs:
+        with pytest.raises(msmhelper.FileError):
+            msmhelper.opentxt_limits(limits_file=limits_file,
+                                     file_name=traj_file)
     else:
         # check with limits file
         expected = [[1, 1, 1, 1, 1], [2, 2, 1, 2, 3], [2, 2, 3]]
