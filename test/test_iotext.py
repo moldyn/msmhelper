32efcf15aff19549bf49a02f85b464c92520dd40
braniii <daniel.nagel@protonmail.com>
2020-05-18 11:28:52 +0200

    Update test functions for iotext.

--- test/test_iotext.py
+++ test/test_iotext.py
@@ -30,18 +30,25 @@ def limits_file():
 
 
 # ~~~ TESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-@pytest.mark.parametrize('traj_file, comment', [
-    (os.path.join(HERE, 'data.dat'), '#'),
-    (os.path.join(HERE, 'traj1.dat'), '#'),
-    (os.path.join(HERE, 'traj1.dat'), ['#']),
-    (os.path.join(HERE, 'traj2.dat'), ['#', '@'])])
-def test_opentxt(traj_file, comment):
+@pytest.mark.parametrize('traj_file, kwargs', [
+    (os.path.join(HERE, 'data.dat'), {'comment': '#'}),
+    (os.path.join(HERE, 'data.dat'), {'usecols': (1, 0)}),
+    (os.path.join(HERE, 'traj1.dat'), {}),
+    (os.path.join(HERE, 'traj1.dat'), {'comment': ['#']}),
+    (os.path.join(HERE, 'traj2.dat'), {'comment': ['#', '@']})])
+def test_opentxt(traj_file, kwargs):
     """Test that a file is opened correctly."""
-    expected = [1, 1, 1, 1, 1, 2, 2, 1, 2, 3, 2, 2, 3]
-    data = msmhelper.opentxt(traj_file, comment=comment)
-    if len(data.shape) > 1:
-        data = data[:, 0]
-    assert (data == expected).all()
+    expected = np.array([[1, 2], [1, 3], [1, 2], [1, 1], [1, 2], [2, 1],
+                         [2, 5], [1, 4], [2, 3], [3, 2], [2, 1], [2, 2],
+                         [3, 3]])
+    data = msmhelper.opentxt(traj_file, **kwargs)
+    if len(data.shape) == 1:
+        assert (data == expected[:, 0]).all()
+    else:
+        if 'usecols' in kwargs:
+            expected[:, (0, 1)] = expected[:, kwargs['usecols']]
+        for i, row in enumerate(data):
+            assert (row == expected[i]).all()
 
 
 @pytest.mark.parametrize('traj_file, header', [
@@ -71,25 +78,42 @@ def test_open_limits(limits_file, wrong_limit):
     (os.path.join(HERE, 'traj1.dat'), {'dtype': np.integer}),
     (os.path.join(HERE, 'traj1.dat'), {'dtype': np.float}),
     (os.path.join(HERE, 'data.dat'), {'no_traj': True})])
-def test_opentxt_limits(limits_file, traj_file, kwargs):
+def test_openmicrostates(limits_file, traj_file, kwargs):
     """Test that the trajectory is split correctly."""
     if 'dtype' in kwargs and kwargs['dtype'] == np.float:
         with pytest.raises(TypeError):
-            msmhelper.opentxt_limits(limits_file=limits_file,
-                                     file_name=traj_file, **kwargs)
+            msmhelper.openmicrostates(limits_file=limits_file,
+                                      file_name=traj_file, **kwargs)
     elif 'no_traj' in kwargs:
         with pytest.raises(msmhelper.FileError):
-            msmhelper.opentxt_limits(limits_file=limits_file,
-                                     file_name=traj_file)
+            msmhelper.openmicrostates(limits_file=limits_file,
+                                      file_name=traj_file)
     else:
         # check with limits file
         expected = [[1, 1, 1, 1, 1], [2, 2, 1, 2, 3], [2, 2, 3]]
-        trajs = msmhelper.opentxt_limits(limits_file=limits_file,
-                                         file_name=traj_file, **kwargs)
+        trajs = msmhelper.openmicrostates(limits_file=limits_file,
+                                          file_name=traj_file, **kwargs)
         for i, traj in enumerate(trajs):
             assert (traj == expected[i]).all()
 
-        # check without limits file
+
+@pytest.mark.parametrize('traj_file, expected', [
+    (os.path.join(HERE, 'traj1.dat'),
+     [[1, 1, 1, 1, 1], [2, 2, 1, 2, 3], [2, 2, 3]]),
+    (os.path.join(HERE, 'data.dat'),
+     [[[1, 2], [1, 3], [1, 2], [1, 1], [1, 2]],
+      [[2, 1], [2, 5], [1, 4], [2, 3], [3, 2]],
+      [[2, 1], [2, 2], [3, 3]]])])
+def test_opentxt_limits(limits_file, traj_file, expected):
+    """Test that the trajectory is split correctly."""
+    # check with limits file
+    trajs = msmhelper.opentxt_limits(limits_file=limits_file,
+                                     file_name=traj_file)
+    for i, traj in enumerate(trajs):
+        assert (traj == expected[i]).all()
+
+    # check without limits file
+    if len(trajs[0].shape) == 1:
         expected = [[1, 1, 1, 1, 1, 2, 2, 1, 2, 3, 2, 2, 3]]
         trajs = msmhelper.opentxt_limits(file_name=traj_file)
         for i, traj in enumerate(trajs):
