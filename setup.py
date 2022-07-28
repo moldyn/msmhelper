a43a5df21dd3d9b5d45c6a8b1de57cecafa2a863
braniii <daniel.nagel@protonmail.com>
2022-07-28 17:21:16 +0200

    Add py3.10 flag to meta data

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     packages=setuptools.find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
