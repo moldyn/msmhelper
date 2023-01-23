4327d346638c148820ea1a42de2fad27c93fa2b0
braniii <daniel.nagel@protonmail.com>
2023-01-23 13:14:11 +0100

    Shift CK- and BH-test to msm submodule.

--- src/msmhelper/msm/__init__.py
+++ src/msmhelper/msm/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """  --8<-- "README.md" """
 
+from .tests import (
+    bh_test,
+    buchete_hummer_test,
+    chapman_kolmogorov_test,
+    ck_test,
+)
 from .msm import (
     estimate_markov_model,
     equilibrium_population,
