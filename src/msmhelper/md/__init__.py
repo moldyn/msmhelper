cd276c439e09c4d32c1cee9a72039d8e7974804c
braniii <daniel.nagel@protonmail.com>
2023-02-10 14:36:28 +0100

    Improve __init__ docstrings. [2/?]

--- src/msmhelper/md/__init__.py
+++ src/msmhelper/md/__init__.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
-"""  --8<-- "README.md" """
+"""# Time Series Analysis
+
+This submodule offers techniques for the analysis of state trajectories&mdash;commonly known as Molecular Dynamics (MD)&mdash;without relying on Markov state models. It encompasses functions for determining timescales, recognizing significant events, correcting dynamical anomalies, and evaluating various state discretization methods.  These functions provide a comprehensive solution for analyzing time-series data and understanding the underlying dynamics of complex systems.
+
+The submodule is structured into the following submodules:
+
+- [**comparison:**][msmhelper.md.comparison] This submodule holds methods to quantify the similarity of different state discretizations.
+- [**corrections:**][msmhelper.md.corrections] This submodule provides an implementation of dynamical coring.
+- [**timescales:**][msmhelper.msm.timescales] This submodule contains methods for estimating various timescales based on discrete time series.
+
+
+"""
+__all__ = [
+    'compare_discretization',
+    'dynamical_coring',
+    'estimate_waiting_times',
+    'estimate_wt',
+    'estimate_paths',
+]
 
 from .comparison import compare_discretization
 from .corrections import dynamical_coring
 from .timescales import (
     estimate_waiting_times,
     estimate_wt,
-    estimate_paths
+    estimate_paths,
 )
--- src/msmhelper/msm/__init__.py
+++ src/msmhelper/msm/__init__.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
-"""  --8<-- "README.md" """
+"""# Markov State Modeling
 
+This submodule contains methods related to Markov state modeling, a powerful technique for analyzing complex systems. It provides a set of functions for constructing and analyzing Markov models, including methods for calculating transition probabilities and estimating various time scales.
+
+The submodule is structured into the following submodules:
+
+- [**msm:**][msmhelper.msm.msm] This submodule contains all methods related to estimate the Markov state model.
+- [**tests:**][msmhelper.msm.tests] This submodule holds methods for validating Markov state models.
+- [**timescales:**][msmhelper.msm.timescales] This submodule contains methods for estimating various timescales based on a Markov model.
+- [**utils:**][msmhelper.msm.utils] This submodule provides some useful linear algebra methods.
+
+"""
+__all__ = [
+    'bh_test',
+    'buchete_hummer_test',
+    'chapman_kolmogorov_test',
+    'ck_test',
+    'estimate_markov_model',
+    'equilibrium_population',
+    'peq',
+    'row_normalize_matrix',
+    'implied_timescales',
+    'estimate_waiting_times',
+    'estimate_wt',
+]
 from .tests import (
     bh_test,
     buchete_hummer_test,
