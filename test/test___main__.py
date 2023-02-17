b7c8eaa793d0eddeaf6751781535a5fe034fe9be
braniii <daniel.nagel@protonmail.com>
2023-02-17 16:36:35 +0100

    Add test for implied timescales cli.

--- test/test___main__.py
+++ test/test___main__.py
@@ -41,6 +41,38 @@ def test_ck_test(tmpdir):
     runner = CliRunner()
 
     # create trajectories
+    traj = np.loadtxt('test/assets/8state_microtraj')
+    macrotraj = np.loadtxt('test/assets/8state_macrotraj')
+    trajfile = tmpdir.join('traj')
+    macrotrajfile = tmpdir.join('macrotraj')
+    np.savetxt(trajfile, traj, fmt='%.0f')
+    np.savetxt(macrotrajfile, macrotraj, fmt='%.0f')
+
+    params = (
+        '--lagtimes 1 2 3 4 5 --frames-per-unit 1 '
+        '--unit frames --grid 2 2 --max-time 500 '
+    )
+
+    result = runner.invoke(
+        main,
+        f'ck-test {params} --filename {macrotrajfile}'.split(),
+    )
+    assert result.exit_code == 0
+
+    result = runner.invoke(
+        main,
+        (
+            f'ck-test {params} --filename {macrotrajfile} '
+            f'--microfilename {trajfile}'
+        ).split(),
+    )
+    assert result.exit_code == 0
+
+
+def test_implied_timescale(tmpdir):
+    runner = CliRunner()
+
+    # create trajectories
     traj, macrotraj = datasets.hummer15_8state(
         0.2, 0.05, 50000, return_macrotraj=True,
     )
@@ -49,23 +81,19 @@ def test_ck_test(tmpdir):
     np.savetxt(trajfile, traj, fmt='%.0f')
     np.savetxt(macrotrajfile, macrotraj, fmt='%.0f')
 
+    params = '--max-lagtime 25 --frames-per-unit 1 --unit frames'
+
     result = runner.invoke(
         main,
-        (
-            'ck-test --lagtimes 1 2 3 4 5 --frames-per-unit 1 '
-            '--unit frames --grid 2 2 --max-time 500 '
-            f'--filename {macrotrajfile}'
-        ).split(),
+        f'implied-timescales {params} --filename {macrotrajfile}'.split(),
     )
     assert result.exit_code == 0
 
     result = runner.invoke(
         main,
         (
-            'ck-test --lagtimes 1 2 3 4 5 --frames-per-unit 1 '
-            '--unit frames --grid 2 2 --max-time 500 '
-            f'--filename {macrotrajfile} --microfilename {trajfile}'
+            f'implied-timescales {params} --filename {macrotrajfile} '
+            f'--microfilename {trajfile}'
         ).split(),
     )
-    print(result.output)
     assert result.exit_code == 0
