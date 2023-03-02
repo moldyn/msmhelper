Fix wrong normalization of waiting time distribution.

src/msmhelper/msm/timescales.py
@@ -203,7 +203,7 @@ def _estimate_times(
     for time, count in ts.items():
         pts[time] = count
     return (
-        pts / pts.sum(),
+        pts / (pts.sum() * lagtime),
         np.arange(len(pts) + 1) * lagtime,
     )
 

test/test_msm_timescales.py
@@ -232,7 +232,7 @@ def test_propagate_MCMC(trajs, lagtime, steps, start, error, rand):
             1,
             2,
             False,
-            (np.array([0, 10, 3, 0, 1]) / 14, [0, 2, 4, 6, 8]),
+            (np.array([0, 10, 3, 0, 1]) / 14 / 2, [0, 2, 4, 6, 8]),
             None,
         ),
         (